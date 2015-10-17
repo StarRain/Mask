d5342990f8648a71dd32258e89218b9cf5718b20
momodae <bingo110@126.com>
2015-10-17 16:30:48 +0800

    提交所有mask示例代码

---
+++ app/src/main/java/com/ldfs/mask/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ldfs.mask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ldfs.mask.guide.GuideManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_show).setOnClickListener(v -> GuideManager.get().show(this, this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
